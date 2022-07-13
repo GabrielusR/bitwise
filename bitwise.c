af92bd76f5346b3b50e439c3aa99f03131de45e6
Gabriel Ramos <gabrielramos1510@gmail.com>
2022-07-13 17:22:27 -0300

    Adding bit flipping function

--- bitwise.c
+++ bitwise.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h>
 #include <stdint.h>
 
 #include "dbg.h"
@@ -21,6 +18,11 @@ void set_unused(uint8_t *byte, int bit)
 	*byte &= ~(1<<bit);
 }
 
+void flip_bit(uint8_t *byte, int bit)
+{
+	*byte ^= (1<<bit);
+}
+
 int main (int argc, char *argv[])
 {
 	uint8_t byte = 0;	
@@ -38,13 +40,21 @@ int main (int argc, char *argv[])
 
 		TEST( (bit >= 0 && bit <= 8), "Number entered is invalid" );
 		
+		/*
+		 
 		if( is_in_use(byte, bit) ){
 			printf("Bit in use. Setting bit down.\n\n");
 			set_unused(&byte, bit);
 		} else {
 			printf("Bit not in use. Setting bit up.\n\n");
 			set_in_use(&byte, bit);
-		}	
+		}
+				OR
+
+			We can just do... flip_bit
+		*/	
+
+		flip_bit(&byte, bit);
 
 		printf("byte: %08b\n\n", byte);
 	}
